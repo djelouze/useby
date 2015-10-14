53e694da806aa0c7f201c5cea0694051b28bbbf8
Jerome Velut <jerome.velut@kitware.com>
2015-10-14 13:11:42 +0200

    Windows dll export

--- useby.h
+++ useby.h
@@ -2,12 +2,18 @@
 #define USEBY_H
 #include <string>
 
+#ifdef WIN32
+#define WIN32_EXPORT __declspec(dllexport)
+#else
+#define WIN32_EXPORT
+#endif
+
 typedef std::string (*AuthentificateFunc)();
 typedef bool (*AuthorizeFunc)();
 
 extern "C"{
-std::string authentificate();
-bool authorize();
+std::string WIN32_EXPORT authentificate();
+bool WIN32_EXPORT authorize();
 }
 
 #endif // USEBY_H
